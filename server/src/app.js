4ebd38fc5b0d81a923ee11fe40c344ef53e43e85
Dennis Xiao <dennisboys@gmail.com>
2018-04-16 22:33:46 +1000

    set up a simple express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,22 @@
-console.log('hello')
+console.log('Yo mate, your server is up and running')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+// use middleware
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// define routes
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
